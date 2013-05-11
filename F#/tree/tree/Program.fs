992f07bb87053b5682eed732b64621fbb020556c
AlexMitenev <xawu@yandex.ru>
2013-05-11 20:12:40 +0400

    Update tree. add print

--- F#/tree/tree/Program.fs
+++ F#/tree/tree/Program.fs
@@ -1,14 +1,15 @@
-﻿//Alex Mitenev 2012
+//Alex Mitenev 2013
 
 type Tree =
   | Empty
   | Node of int * Tree * Tree
 
-let rec addElement tree n =
+let rec addElement n tree =
   match tree with
     | Empty -> Node(n, Empty, Empty)
-    | Node(value, left, right) when n < value -> Node(value, addElement left n, right)
-    | Node(value, left, right) when n > value -> Node(value, left, addElement right n)
+    | Node(value, left, right) when n < value -> Node(value, addElement n left, right)
+    | Node(value, left, right) when n > value -> Node(value, left, addElement n right)
+    |Node(value, left, right) when n = value -> Node(value, left, right)
 
 let rec find tree n =
   match tree with
@@ -23,45 +24,47 @@ let rec findMin tree =
       | Node(value, left, right) -> findMin left
 
 
-let rec deleteElement tree n =
+let rec deleteElement n tree =
   match tree with
     | Empty -> Empty
-    | Node(value, left, right) when n < value -> Node(value, deleteElement left n, right)
-    | Node(value, left, right) when n > value -> Node(value, left, deleteElement right n)
+    | Node(value, left, right) when n < value -> Node(value, deleteElement n left, right)
+    | Node(value, left, right) when n > value -> Node(value, left, deleteElement n right)
 
     | Node(value, Empty, Empty) when n = value -> Empty
     | Node(value, Empty, Node(valNext, leftNext, rightNext)) when n = value -> Node(valNext, leftNext, rightNext)
     | Node(value, Node(valNext, leftNext, rightNext), Empty) when n = value -> Node(valNext, leftNext, rightNext)
-    | Node(value, left,  right) when n = value -> Node(findMin right, left, deleteElement right <|findMin right)
+    | Node(value, left,  right) when n = value -> Node(findMin right, left, deleteElement (findMin right) right)
 
 let rec printSpases num =
   match num with
     |0 -> printf "%s" ""
-    |n when n > 0 -> (printf "%c" ' ')
+    |n when n < 0 -> printSpases 0
+    |n when n > 0 -> printf "%c" ' '
                      printSpases (n - 1)
 
 let rec print tree offset = 
-  printSpases offset
+  printSpases (offset - 2)
+  if offset <> 0 then
+    printf "%c" '|'
+    printf "%c" '_'
+    printf "%c" '_'
   match tree with
-    | Empty -> printfn "%s" "Nill"
+    | Empty ->printfn "%s" "Nill"
     | Node(value, left, right) -> printfn "%d" value
+                                  //printSpases (offset)
+                                  //printfn "%c" '|'
                                   print left (offset + 2)
                                   print right (offset + 2)
 
-
-//example
-let myTree = Empty
-let myTree1 = addElement myTree 5
-let myTree2 = addElement myTree1 2
-let myTree3 = addElement myTree2 8
-let myTree4 = addElement myTree3 4
-let myTree5 = addElement myTree4 1
-let myTree6 = addElement myTree5 9
-let myTree7 = addElement myTree6 0
-//let myTree8 = deleteElement myTree7 5
-print myTree7 0
-
-(*let t =
- Empty
- |> addElement 5
- |> addElement 2!!!!!!!!!!!!!!!!!*)
+let MyTree =
+  Empty
+  |> addElement 5
+  |> addElement 3
+  |> addElement 0
+  |> addElement 4
+  |> addElement 6
+  |> addElement 2
+  |> addElement 9
+  |> addElement 7
+  |> deleteElement 5
+print MyTree 0
